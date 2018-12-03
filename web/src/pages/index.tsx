8a26110117b37b8bd84ae1109bff1eb0dcc97958
seonghyeon <hyeon@apartmentary.com>
2018-12-03 22:15:25 +0900

    web - add react-router-dom

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React, { PureComponent } from "react";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import Login from "@/pages/login";
 
 class App extends PureComponent {
   public render() {
     return (
-      <Login />
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Redirect to="/login" />
+        </Switch>
+      </Router>
     );
   }
 }
