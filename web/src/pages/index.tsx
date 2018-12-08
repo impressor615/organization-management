e19fd1a3ea5a62be0c96f6eec0d81ec6278420bb
seonghyeon <hyeon@apartmentary.com>
2018-12-08 21:18:44 +0900

    web - add ErrorAlert to app

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { Fragment, PureComponent } from "react";
 import { Provider } from "react-redux";
 import {
   HashRouter as Router,
@@ -7,6 +7,7 @@ import {
   Switch,
 } from "react-router-dom";
 
+import ErrorAlert from "@/components/ErrorAlert";
 import configureStore from "@/configureStore";
 import Dashboard from "@/pages/dashboard";
 import Login from "@/pages/login";
@@ -18,12 +19,15 @@ class App extends PureComponent {
     return (
       <Provider store={Store}>
         <Router>
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Redirect to="/login" />
-          </Switch>
+          <Fragment>
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Redirect to="/login" />
+            </Switch>
+            <ErrorAlert />
+          </Fragment>
         </Router>
       </Provider>
     );
