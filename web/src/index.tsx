5882fd75f939f78a35a4b14838d396a521aa970b
seonghyeon <hyeon@apartmentary.com>
2018-12-09 16:45:15 +0900

    web - add fontAwesome

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,14 @@
 import "@/assets/scss/main.scss";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSitemap } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "@/pages";
 
+const faIcons = [faSitemap];
+library.add(...faIcons);
 ReactDOM.render(
     <App />,
     document.getElementById("app"),
