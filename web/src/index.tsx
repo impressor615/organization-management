e6b7afb2c0c7ed3121726971b1e683a11e79019f
seonghyeon <hyeon@apartmentary.com>
2018-12-14 19:50:57 +0900

    web - add other font-awesome icons

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,13 @@
 import "@/assets/scss/main.scss";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPlus, faSitemap } from "@fortawesome/free-solid-svg-icons";
+import { faCaretDown, faCaretUp, faPlus, faSitemap } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "@/pages";
 
-const faIcons = [faSitemap, faPlus];
+const faIcons = [faSitemap, faPlus, faCaretDown, faCaretUp];
 library.add(...faIcons);
 ReactDOM.render(
     <App />,
