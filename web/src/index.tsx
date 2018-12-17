087ef3590d42923e10fbb20f10e7f6ac3b3c4db8
seonghyeon <hyeon@apartmentary.com>
2018-12-17 16:50:52 +0900

    web - add icons

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,13 +1,27 @@
 import "@/assets/scss/main.scss";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCaretDown, faCaretUp, faPlus, faSitemap } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCaretDown,
+  faCaretUp,
+  faPlus,
+  faSearch,
+  faSitemap,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "@/pages";
 
-const faIcons = [faSitemap, faPlus, faCaretDown, faCaretUp];
+const faIcons = [
+  faSitemap,
+  faPlus,
+  faCaretDown,
+  faCaretUp,
+  faSearch,
+  faUser,
+];
 library.add(...faIcons);
 ReactDOM.render(
     <App />,
