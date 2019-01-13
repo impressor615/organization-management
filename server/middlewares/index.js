3b86d6292aa1e4cced819a8e57d9445236175e3a
seonghyeon <hyeon@apartmentary.com>
2019-01-13 23:41:09 +0900

    server - add compression middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 const parseLanguage = require('./parseLanguage');
 const pagination = require('./pagination');
@@ -11,6 +12,7 @@ module.exports = (app) => {
     app.use(morgan('combined'));
   }
   app.use(bodyParser.json());
+  app.use(compression());
   app.use(parseLanguage);
   app.use(pagination);
   app.use(/^\/api\/user\/teams.*$/, auth);
